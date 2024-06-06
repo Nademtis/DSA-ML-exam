99bda783bb964ed95695fa10abcdb6b847a0edd8
Nademtis <mathias.99.madsen@gmail.com>
2024-06-06 19:46:40 +0200

    player movement NOW WORKS WITH CAPS

--- managerScripts/inputManger.js
+++ managerScripts/inputManger.js
@@ -13,10 +13,10 @@ export default class InputManger {
     }
     keyPress(event) {
         switch (event.key) {
-            case "d": this.player.controls.right = true; break
-            case "a": this.player.controls.left = true; break
-            case "w": this.player.controls.up = true; break
-            case "s": this.player.controls.down = true; break
+            case "d": case "D": this.player.controls.right = true; break
+            case "a": case "A": this.player.controls.left = true; break
+            case "w": case "W": this.player.controls.up = true; break
+            case "s": case "S": this.player.controls.down = true; break
             //case "e": this.controls.use = true; break
             case "ArrowRight": this.player.shootDirection = "right"; break
             case "ArrowLeft": this.player.shootDirection = "left"; break
@@ -31,10 +31,10 @@ export default class InputManger {
 
     keyUp(event) {
         switch (event.key) {
-            case "d": this.player.controls.right = false; break
-            case "a": this.player.controls.left = false; break
-            case "w": this.player.controls.up = false; break
-            case "s": this.player.controls.down = false; break
+            case "d": case "D": this.player.controls.right = false; break
+            case "a": case "A": this.player.controls.left = false; break
+            case "w": case "W": this.player.controls.up = false; break
+            case "s": case "S": this.player.controls.down = false; break
             //case "e": this.controls.use = false; break
             case "ArrowRight":
             case "ArrowLeft":
